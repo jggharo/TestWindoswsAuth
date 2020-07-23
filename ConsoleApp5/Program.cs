57d33f445f8a1b2e14d324ccd8a992e988b114d1
Gerardo Garcia <gerardo.garcia@NETCheck.es>
2020-07-23 10:14:19 +0200

    first commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://localhost:44321/weatherforecast");
+            client.Authenticator = new NtlmAuthenticator();
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            Console.ReadKey();
+        }
+    }
+}
